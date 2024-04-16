c3c494e55d7044d38067d7b2ed8fe1fa77539338
Celso Bastos <celsorico@gmail.com>
2024-04-15 23:31:19 -0300

    js

---
+++ javascript/bundle.js
@@ -0,0 +1,23 @@
+(function() {
+    "use strict"
+
+    /* Link */
+    const eLink = document.querySelectorAll('[data-js="link"]');
+    eLink.forEach(function(nodeLink) {
+        nodeLink.addEventListener('click', function (event) {
+            event.preventDefault();
+            console.log(event.target.getAttribute('class'));
+        });
+    });
+
+    console.dir(eLink);
+    
+    /** Btn */
+    const el = document.querySelector('[data-js="btn"]');
+    el.addEventListener('click', function(event) {
+        const eModal = document.querySelector('[data-js="modal"]');
+        eModal.classList.add('modal');
+    });
+
+
+})();
